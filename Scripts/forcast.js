e19e27ced9f4fd9681ddd2041e425ead78d7dee2
Whirlwindraven <fadysherif1998@gmail.com>
2023-03-13 20:42:46 -0400

    refracoted and redid the script code for the forcast script file, as well as combined both script file codes to make it easier.

--- Scripts/forcast.js
+++ Scripts/forcast.js
@@ -1,35 +1,36 @@
 const apiKey = 'EPPB4746tUOr12IhKAniCfeJjm3o3Oqv';
 
-
 const getWeather = async (identification) => {
-
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${identification}?apikey=${apiKey}`;
-
     const response = await fetch(base + query);
     const data = await response.json();
-
-    return data[0];
-}
-
-
-
-
-const getPlace = async (city) => {
-
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apikey=${apiKey}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
     return data[0];
 };
 
-getPlace('newyork')
-.then(data => {
-    return getWeather(data.apiKey);
-}).then(data => {
+const getPlace = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${apiKey}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+const updatePlace = async (place) => {
+    const cityDetails = await getPlace(place);
+    const weather = await getWeather(cityDetails.Key);  // use cityDetails.Key as parameter
+    return { cityDetails, weather };
+};
+
+const placeForm = document.querySelector('form');
+placeForm.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    // getting the area value
+    const place = placeForm.place.value.trim();
+    placeForm.reset();
+
+    // update the city value
+    const data = await updatePlace(place);
     console.log(data);
-})
-.catch(error => console.log(error));
+});
