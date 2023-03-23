516339dfa15a2fb7199bf04d23ef692d459f841d
Whirlwindraven <fadysherif1998@gmail.com>
2023-03-23 19:19:29 -0400

    fixed the https request as well as the syntax error'

--- Scripts/forcast.js
+++ Scripts/forcast.js
@@ -4,9 +4,9 @@ const card = document.querySelector('.card');
 const dets = document.querySelector('.details')
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img')
-
+const forecastDiv = document.getElementById('forecast');
 const getWeather = async (identification) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${identification}?apikey=${apiKey}`;
     const response = await fetch(base + query);
     const data = await response.json();
@@ -14,7 +14,7 @@ const getWeather = async (identification) => {
 };
 
 const getPlace = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${apiKey}&q=${city}`;
     const response = await fetch(base + query);
     const data = await response.json();
@@ -26,6 +26,45 @@ const updatePlace = async (place) => {
     const weather = await getWeather(cityDetails.Key);  // use cityDetails.Key as parameter
     return { cityDetails, weather };
 };
+
+const getForecast = async (cityKey) => {
+    const base = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+    const query = `${cityKey}?apikey=${apiKey}&metric=true`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data.DailyForecasts;
+  }
+  
+  const updateForecastUI = (forecasts) => {
+    const forecastContainer = document.querySelector('.forecast');
+    forecastContainer.innerHTML = '';
+  
+    forecasts.forEach(forecast => {
+      const date = new Date(forecast.Date);
+      const dayOfWeek = date.toLocaleString('default', { weekday: 'short' });
+      const iconSrc = `./images/icons/${forecast.Day.Icon}.svg`;
+      const minTemp = forecast.Temperature.Minimum.Value;
+      const maxTemp = forecast.Temperature.Maximum.Value;
+      const windSpeed = forecast.Day.Wind.Speed.Value;
+      const humidity = forecast.Day.Humidity;
+  
+      const forecastCard = document.createElement('div');
+      forecastCard.classList.add('col-md-2');
+      forecastCard.innerHTML = `
+      <div class="card forecast-card">
+        <div class="card-body">
+          <h5 class="card-title">${dayOfWeek}</h5>
+          <img class="forecast-icon" src="${iconSrc}" alt="${forecast.Day.IconPhrase}">
+          <p class="card-text">${minTemp}&deg;C - ${maxTemp}&deg;C</p>
+          <p class="card-text">Wind: ${windSpeed} km/h</p>
+          <p class="card-text">Humidity: ${humidity}%</p>
+        </div>
+      </div>
+    `;
+
+    forecastContainer.appendChild(forecastCard);
+  });
+}
 
 const updateUI = (data) => {
 
@@ -61,25 +100,23 @@ dets.innerHTML = `
 };
 
 placeForm.addEventListener('submit', async e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // getting the area value
-    const place = placeForm.place.value.trim();
-    placeForm.reset();
+  // getting the area value
+  const place = placeForm.place.value.trim();
+  placeForm.reset();
 
-    // update the city value
-    updatePlace(place)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err))
+  // update the city value
+  updatePlace(place)
+      .then(data => updateUI(data))
+      .catch(err => console.log(err));
 
-
-// updating local storage
-localStorage.setItem('location', place);
-
+  // updating local storage
+  localStorage.setItem('location', place);
 });
 
-if(localStorage.getItem('location')){
-    updatePlace(localStorage.getItem('location'))
-    .then(data => updateUI(data))
-    .catch()
+if (localStorage.getItem('location')) {
+  updatePlace(localStorage.getItem('location'))
+      .then(data => updateUI(data))
+      .catch(err => console.log(err));
 }
